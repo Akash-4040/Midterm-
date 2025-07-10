968a56d9752374e8ba9fe6467c7eb351133f3241
Akash Saru Magar <akashsarumagar144@gmail.com>
2025-07-10 02:00:14 -0700

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,12 +1,19 @@
-#pragma once
+=#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
 #include <string>
 using namespace std;
 
 class Student {
 public:
-    int id; string name; float grade;
+    int id;
+    string name;
+    float grade;
 
-    Student() { id = 0; name = ""; grade = 0; }
-    Student(int i, string n, float g) { id = i; name = n; grade = g; }
-    void display() { cout << "ID: " << id << " Name: " << name << " Grade: " << grade << endl; }
+    Student();
+    Student(int i, string n, float g);
+    void display();
 };
+
+#endif
