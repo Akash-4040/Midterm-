f8a2fbcf80785f59d7bd414d280ac6f301fbc174
Akash Saru Magar <akashsarumagar144@gmail.com>
2025-07-10 01:56:56 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <string>
-#include "Student.h"
 #include "GradeBook.h"
 using namespace std;
 
@@ -12,17 +10,24 @@ int main() {
         cout << "\n1. Add Student\n2. Show All\n3. Search by ID\n4. Save\n5. Load\n0. Exit\nChoice: ";
         cin >> choice;
 
-        if (choice == 1) gb.addStudent();
-        else if (choice == 2) gb.displayAll();
+        if (choice == 1)
+            gb.addStudent();
+        else if (choice == 2)
+            gb.displayAll();
         else if (choice == 3) {
             int id;
-            cout << "ID: "; cin >> id;
+            cout << "ID: ";
+            cin >> id;
             int i = gb.binarySearch(id);
-            if (i != -1) gb.showStudent(i);
-            else cout << "Not found.\n";
+            if (i != -1)
+                gb.showStudent(i);
+            else
+                cout << "Not found.\n";
         }
-        else if (choice == 4) gb.saveToFile("students.txt");
-        else if (choice == 5) gb.loadFromFile("students.txt");
+        else if (choice == 4)
+            gb.saveToFile("students.txt");
+        else if (choice == 5)
+            gb.loadFromFile("students.txt");
     } while (choice != 0);
 
     return 0;
